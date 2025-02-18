ca249af16463662b3bb7f029ad3adff1474d3d6e
Idan Levi <29idan29@gmail.com>
2025-02-18 21:49:43 +0200

    fixing production bug

--- client/components/ui/file-upload.tsx
+++ client/components/ui/file-upload.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { IconUpload } from "@tabler/icons-react";
