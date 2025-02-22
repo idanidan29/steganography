22e13805e4f1108fe79db49bf7a5094339ed93ca
Idan Levi <29idan29@gmail.com>
2025-02-22 13:06:00 +0200

    cheking api

--- server/app.py
+++ server/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 # Register Blueprint (Routes)
 app.register_blueprint(stego_bp, url_prefix="/stego")
 
+@app.route("/")
+def home():
+    return "Hello, Flask API is running!"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
