887d33fd70a72973560049f722d0f740a2492071
Idan Levi <29idan29@gmail.com>
2025-02-23 13:07:53 +0200

    adding swich

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,18 +1,20 @@
-"use client"
+"use client";
 
+import { useState } from "react";
 import { Decoder } from "@/components/Decoder";
 import { Encoder } from "@/components/Encoder";
 import Footer from "@/components/Footer";
 import { Hero } from "@/components/Hero";
-
+import { SliderToggle } from "@/components/ui/SliderToggle";
 
 export default function Home() {
+  const [selected, setSelected] = useState<"Encrypt" | "Decrypt">("Encrypt");
+
   return (
     <div className="bg-white">
-      <Hero/>
-      <Encoder/>
-      <Decoder/>
-      <Footer/>
+      <Hero />
+      <SliderToggle selected={selected} setSelected={setSelected} /> {selected === "Encrypt" ? <Encoder /> : <Decoder />}
+      <Footer />
     </div>
   );
 }
